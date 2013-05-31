265bc7e3f869bb1f6804b5b67bd7a8656803a119
Andrew Dryga <andrew@dryga.com>
2013-05-31 22:26:29 +0300

    Refactored code

--- scripts/application/main.js
+++ scripts/application/main.js
@@ -1,15 +1,31 @@
-function redraw() {
-  $('.section').width($(window).width());
-  $('.section').height($(window).height());
-
+$(function() {
+  // Selectors
+  var $window = $(window);
+  var $document = $(document);
   var $sections = $('.section');
   var $nav_prev = $('.nav-prev');
   var $nav_next = $('.nav-next');
-  var section_height = $sections.first().height();
-  var last_section_offset = $sections.last().offset().top;
+  var $first_section = $sections.first();
+  var $last_section = $sections.last();
 
-  var getCurrentSection = function(viewport_offser) {
-    var offset = $(window).scrollTop() + (viewport_offser ? viewport_offser : 0);
+  // Params
+  var section_height = $first_section.height();
+  var first_section_offset = 0;
+  var last_section_offset = $last_section.offset().top;
+
+  // Methods
+  function rescaleSections() {
+    $sections.width($window.width());
+    $sections.height($window.height());
+
+    // Recalc params
+    section_height = $first_section.outerHeight();
+    first_section_offset = 0;
+    last_section_offset = $last_section.offset().top;
+  }
+
+  function getCurrentSection(viewport_offset) {
+    var offset = $(window).scrollTop() + (viewport_offset ? viewport_offset : 0);
     var section;
     $sections.each(function() {
       var $this = $(this);
@@ -19,32 +35,16 @@ function redraw() {
       section = $this;
     });
     return section;
-  };
+  }
 
-  $nav_prev.off().click(function() {
-    var prev_section = getCurrentSection($(window).height()/2).prev('.section');
-    var prev_section_offset = prev_section.length > 0 ? prev_section.offset().top : 0;
-    $('body,html').animate({scrollTop: prev_section_offset + 'px'}, 800);
-  });
-
-  $nav_next.off().click(function() {
-    var next_section = getCurrentSection().next('.section');
-    var last_section_offset = next_section.length > 0 ? next_section.offset().top : $sections.last().offset().top;
-    $('body,html').animate({'scrollTop': last_section_offset + 'px'}, 800);
-  });
-
-  $nav_prev.css({
-    'top': (section_height + 5) + 'px',
-    'opacity': 0
-  });
-
-  $(window).off().scroll(function() {
+  function scrollSpy() {
     var offset = $(window).scrollTop();
+
+    // Top nav element
     var nav_prev_offset = section_height > offset ? section_height - offset : 0;
+    $nav_prev.css('top', nav_prev_offset + 'px');
+
     var nav_prev_opacity = section_height > offset ? offset/section_height : 1;
-
-    $nav_prev.css('top', (nav_prev_offset + 5) + 'px');
-
     if(nav_prev_opacity >= 1) {
       $nav_prev.addClass('fixed');
       $nav_prev.css('opacity', '');
@@ -53,31 +53,57 @@ function redraw() {
       $nav_prev.css('opacity', nav_prev_opacity*0.5);
     }
 
-    var nav_next_offset = last_section_offset - offset - 3;
-    var nav_next_opacity = nav_next_offset / section_height;
-    nav_next_offset -= section_height;
-
-    if(nav_next_offset > 0) {
-      $nav_next.css('bottom', '5px');
+    // Bottom nav element
+    var nav_next_offset = last_section_offset - section_height - offset;
+    if(nav_next_offset >= 0) {
+      $nav_next.css('bottom', '0');
     } else {
-      $nav_next.css('bottom', -1 * nav_next_offset + 5 + 'px');
+      $nav_next.css('bottom', -1 * nav_next_offset + 'px');
     }
 
+    var nav_next_opacity = 1 + nav_next_offset / section_height;
     if(nav_next_opacity >= 1) {
       $nav_next.addClass('fixed');
       $nav_next.css('opacity', '');
-    } else {
+    } else if(nav_next_opacity >= 0) {
       $nav_next.removeClass('fixed');
       $nav_next.css('opacity', nav_next_opacity*0.5);
+    } else {
+      $nav_next.removeClass('fixed');
+      $nav_next.css('opacity', 0);
     }
-  });
+  }
 
-  $.stellar({
-    horizontalScrolling: false,
-    verticalOffset: 7,
-    scrollProperty: 'transform'
-  });
-}
+  // Fix widths
+  rescaleSections();
+  $(window).resize(rescaleSections);
+  $(window).load(rescaleSections);
 
-$(window).resize(redraw);
-$(redraw);
+  // Navigation
+  (function() {
+    $nav_prev.click(function() {
+      var prev_section = getCurrentSection($(window).height()/2).prev('.section');
+      var prev_section_offset = prev_section.length > 0 ? prev_section.offset().top : first_section_offset;
+      $('body,html').animate({scrollTop: prev_section_offset + 'px'}, 800);
+    });
+
+    $nav_next.click(function() {
+      var next_section = getCurrentSection().next('.section');
+      var last_section_offset = next_section.length > 0 ? next_section.offset().top : last_section_offset;
+      $('body,html').animate({'scrollTop': last_section_offset + 'px'}, 800);
+    });
+  })();
+
+  // Scroll spy
+  (function() {
+    $nav_prev.css({
+      'top': section_height + 'px',
+      'opacity': 0
+    });
+
+    scrollSpy();
+    $(window).scroll(scrollSpy);
+    $(window).load(scrollSpy);
+    $(window).resize(scrollSpy);
+  })();
+});
